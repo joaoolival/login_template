23b2b7ed00fb285cd2923a8d9f8b1c31b92ff48e
joaoolival <joaoljolival@gmail.com>
2020-11-19 01:11:53 +0000

    auth email working

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+  if(Auth::check()){
+    return redirect()->route('home');
+  }else{
     return view('welcome');
-})->middleware(['auth','verified']);
+  }
+});
 
 
-Route::view('home','home')->middleware(['auth','verified']);
+Route::view('/home','home')->middleware(['auth','verified'])->name('home');;
